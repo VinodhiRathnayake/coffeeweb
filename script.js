634d2e0f925366437faa1485d944e55095d0a6ac
Vinodhi Rathnayake <vinodhirathnayake30@gmail.com>
2023-11-26 21:01:47 +0530

    update

--- script.js
+++ script.js
@@ -2,17 +2,29 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menubtn').onclick = () => {
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
 let searchForm = document.querySelector('.searchform');
 
 document.querySelector('#searchbtn').onclick = () => {
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
 let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cartbtn').onclick = () => {
     cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');    
+
+}
