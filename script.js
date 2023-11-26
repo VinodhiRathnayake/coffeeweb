be1e1e25af118152a998d9221670d1bca3ba27e0
Vinodhi Rathnayake <vinodhirathnayake30@gmail.com>
2023-11-26 16:35:18 +0530

    update

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ document.querySelector('#menubtn').onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let searchForm = document.querySelector('.searchform');
+
+document.querySelector('#searchbtn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
 let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cartbtn').onclick = () => {
     cartItem.classList.toggle('active');
 }
+
